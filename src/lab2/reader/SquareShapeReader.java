85e59a5f0c48d039eaa05ef21aeda6b43100dece
Aleksey Bartashevich <alexey.barton@gmail.com>
2019-03-23 16:38:16 +0300

    first commit

---
+++ src/lab2/reader/SquareShapeReader.java
@@ -0,0 +1,14 @@
+package lab2.reader;
+
+
+import lab2.shape.Shape;
+
+import java.io.IOException;
+
+public class SquareShapeReader implements ShapeReader {
+
+    @Override
+    public Shape readShape() throws IOException {
+        return null;
+    }
+}
