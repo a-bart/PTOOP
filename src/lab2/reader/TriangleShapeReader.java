e95d4ccd7fd2883e0aeeaa1fe8766046191f69ec
Aleksey Bartashevich <alexey.barton@gmail.com>
2019-03-23 16:57:35 +0300

    feat: handle triangle data enter

--- src/lab2/reader/TriangleShapeReader.java
+++ src/lab2/reader/TriangleShapeReader.java
@@ -1,14 +1,24 @@
 package lab2.reader;
 
-
+import lab2.shape.Triangle;
 import lab2.shape.Shape;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class TriangleShapeReader implements ShapeReader {
 
     @Override
     public Shape readShape() throws IOException {
-        return null;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("enter first side: ");
+        int firstSide = Integer.valueOf(bufferedReader.readLine());
+        System.out.println("enter second side: ");
+        int secondSide = Integer.valueOf(bufferedReader.readLine());
+        System.out.println("enter third side: ");
+        int thirdSide = Integer.valueOf(bufferedReader.readLine());
+        bufferedReader.close();
+        return new Triangle(firstSide, secondSide, thirdSide);
     }
 }
