1aa3283bbcd7e656797037b8cd1e5e012670b0c7
Aleksey Bartashevich <alexey.barton@gmail.com>
2019-03-25 09:43:03 +0300

    add gradle to project

---
+++ src/main/java/lab3/repository/BsonFileShapeRepositoryImpl.java
@@ -0,0 +1,27 @@
+package lab3.repository;
+
+import main.java.lab3.shape.Shape;
+
+import java.util.List;
+
+public class BsonFileShapeRepositoryImpl implements ShapeRepository {
+
+    @Override
+    public void save(Shape shape) {
+    }
+
+    @Override
+    public void update(Shape shape) {
+
+    }
+
+    @Override
+    public void remove(String id) {
+
+    }
+
+    @Override
+    public List<Shape> getAll() {
+        return null;
+    }
+}
